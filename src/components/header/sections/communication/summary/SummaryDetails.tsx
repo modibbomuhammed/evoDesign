ac99f0a60d3be068072833955b38269ee39958a9
Muhammed Modibbo <modibbomuhammed@gmail.com>
2021-05-01 10:42:51 +0100

    Add Summary Component

---
+++ src/components/header/sections/communication/summary/SummaryDetails.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import styled from "styled-components";
+
+import { SummaryBox } from "./SummaryBox";
+import { Activity } from "../CommunicationDetails";
+
+const Details = styled.div`
+  display: flex;
+  width: 559px;
+  height: 95px;
+  justify-content: space-between;
+  background-color: #edf5ff;
+`;
+
+export const SummaryDetails = (props: { activity: Activity }): JSX.Element => {
+  const activities = Object.entries(props.activity);
+  const summary = activities.map(([type, value], index) => (
+    <SummaryBox key={index} value={value} type={type.toUpperCase()} />
+  ));
+  return <Details>{summary}</Details>;
+};
